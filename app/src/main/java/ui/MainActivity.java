6e4638330a76b2ce6c07c81e55adff8ef62296ad
fawazmezher <fawazmezher@hotmail.com>
2023-06-01 19:46:45 -0400

    commit: unfinshed

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,54 +21,32 @@ import algonquin.cst2335.mezh0013.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private MainViewModel model;
     private ActivityMainBinding variableBinding;
+    private ImageView imgView;
+    private Switch sw;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        model = new ViewModelProvider(this).get(MainViewModel.class);
+        setContentView(R.layout.activity_main);
 
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.switch1);
 
-        variableBinding.mybutton.setOnClickListener(click ->
-        {
-            model.editString.postValue(variableBinding.myedittext.getText().toString());
-        });
-        model.editString.observe(this, s -> {
-            variableBinding.textview.setText("Your edit text has: " + s);
-        });
 
-        model.isSelected.observe(this, selected -> {
-            variableBinding.CheckBox.setChecked(selected);
-            variableBinding.RadioButton.setChecked(selected);
-            variableBinding.Switch.setChecked(selected);
+        sw.setOnCheckedChangeListener((btn, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-            String message = "The value is now: " + selected;
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                imgView.startAnimation(rotate);
+            } else {
+                imgView.clearAnimation();
+            }
         });
-        variableBinding.CheckBox.setOnCheckedChangeListener((CheckBox, isChecked)-> {
-            model.isSelected.postValue(variableBinding.CheckBox.isChecked());
-        });
-
-        variableBinding.RadioButton.setOnCheckedChangeListener((RadioButton, isChecked)-> {
-            model.isSelected.postValue(variableBinding.RadioButton.isChecked());
-        });
-
-        variableBinding.Switch.setOnCheckedChangeListener((Switch, isChecked)-> {
-            model.isSelected.postValue(variableBinding.Switch.isChecked());
-        });
-
-        variableBinding.ImageView.setOnClickListener(view -> {
-            Toast.makeText(getApplicationContext(), "ImageView clicked", Toast.LENGTH_SHORT).show();
-        });
-        variableBinding.ImageButton.setOnClickListener(view -> {
-            int width = view.getWidth();
-            int height = view.getHeight();
-            String message = "The width = " + width + " and height = " + height;
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-        });
-
     }
-
 }
+
+
