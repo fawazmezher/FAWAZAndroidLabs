08bee1e038b1795d3bf306b33780fb87ba781ad9
fawazmezher <fawazmezher@hotmail.com>
2023-06-02 01:08:52 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -21,16 +21,31 @@ import algonquin.cst2335.mezh0013.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private MainViewModel model;
     private ActivityMainBinding variableBinding;
-
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imgView = findViewById(R.id.flagView);
+        sw = findViewById(R.id.spin_switch);
 
-        }
+        sw.setOnCheckedChangeListener((btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            }else {
+                imgView.clearAnimation();
+            }
+        });
     }
-
+}
 
 
