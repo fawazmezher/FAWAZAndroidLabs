6b946cf4381e03e497cad5e5a34fe08a239b53eb
fawazmezher <fawazmezher@hotmail.com>
2023-07-27 18:59:12 -0400

    Lab 8.1

--- app/src/main/java/ui/MessageDetailsFragment.java
+++ app/src/main/java/ui/MessageDetailsFragment.java
@@ -1,2 +1,8 @@
-package ui;public class MessageDetailsFragment {
+package ui;
+
+import androidx.fragment.app.Fragment;
+
+public class MessageDetailsFragment  extends Fragment {
+
+    @Override
 }
